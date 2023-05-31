fb215908d0b754b3ad075b02167bc3297382a9fb
nursich <nurlibaiem04@mail.ru>
2023-05-31 23:46:06 +0600

    Implementing methods in class "Search"

---
+++ src/Search.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Search<Vertex> {
+    private WeightedGraph<Vertex> graph;
+
+    public Search(WeightedGraph<Vertex> graph) {
+        this.graph = graph;
+    }
+
+    public boolean pathExist(Vertex source, Vertex destination) {
+        Set<Vertex> visited = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+
+        visited.add(source);
+        queue.offer(source);
+
+        while (!queue.isEmpty()) {
+            Vertex current = queue.poll();
+
+            if (current.equals(destination)) {
+                return true;
+            }
+
+            List<Edge<Vertex>> adjacentEdges = graph.getAdjacentEdges(current);
+            for (Edge<Vertex> edge : adjacentEdges) {
+                Vertex neighbour = edge.getDestination();
+                if (!visited.contains(neighbour)) {
+                    visited.add(neighbour);
+                    queue.offer(neighbour);
+                }
+            }
+
+        }
+        return false;
+    }
+}
