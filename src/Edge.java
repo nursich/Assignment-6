a9e139dc9af20db7d6cb2590f53d215a52dddeda
nursich <nurlibaiem04@mail.ru>
2023-05-31 23:28:12 +0600

    Creating class "Edge"

---
+++ src/Edge.java
@@ -0,0 +1,24 @@
+public class Edge<Vertex> {
+
+    private Vertex source;
+    private Vertex dest;
+    private double weight;
+
+    public Edge(Vertex source, Vertex dest, int weight) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    public Vertex getSource() {
+        return source;
+    }
+
+    public Vertex getDestination() {
+        return dest;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
